0b31414edad6a4e6c42f70734b7a9e73403fc711
Your Name <you@example.com>
2025-02-23 23:33:54 +0100

    initial implementation of the data loader

--- src/dataset/data_loader.py
+++ src/dataset/data_loader.py
@@ -1,22 +1,32 @@
-'''from datatrove.pipeline.readers import ParquetReader
-from tokenizers import Tokenizer
-
-data_reader = ParquetReader("hf://datasets/HuggingFaceFW/fineweb-edu/sample/10BT", limit=1) 
-
-tn = Tokenizer.from_pretrained('gpt2')
-
-max_tokens = 100_000_000
-
-for document in data_reader():
-    print(document.metadata['token_count'] == len(tn.encode(document.text).ids))
-'''
-
 import numpy as np
-import tiktoken
+import random
+import torch
 
-tn = tiktoken.get_encoding('gpt2')
+class DataLoader:
+    def __init__(self, batch_size: int, block_size: int):
+        self.B = batch_size
+        self.T = block_size
 
-farr = np.load('data/shard_it_023.npy')
-larr = np.load('data/shard_en_081.npy')
+    def get_batch(self) -> tuple[torch.Tensor, torch.Tensor]:
+        '''
+        get a batch of dataset and each batch has block_size tokens
+        '''
 
-print(farr[:10])
+        x_shard = random.randrange(0, 100) # do not include shard 101 because there a too few tokens inside
+        type_shard = random.choice(['en', 'it'])
+        path_name_shard = f'data/shard_{type_shard}_{x_shard:03d}.npy'
+    
+        tks_np = np.load(path_name_shard)
+        idxs = torch.randint(0, len(tks_np) - self.T, size=(self.B,))
+
+        # xb = torch.stack([torch.from_numpy(tks_np[idx:idx + block_size]) for idx in idxs], dim=0) 
+        # yb = torch.stack([torch.from_numpy(tks_np[idx + 1:idx + block_size + 1]) for idx in idxs], dim=0)
+
+        # this is much faster implementation of the one above, use less .from_numpy() and do not use for loops so it does not iterate B times, for very high batch_size can be exponentially faster
+        rows = np.arange(self.B)[:, None]
+        cols = np.arange(self.T)
+
+        xb = torch.from_numpy(tks_np[idxs[rows] + cols]) # (B, T)
+        yb = torch.from_numpy(tks_np[idxs[rows] + cols + 1]) # (B, T)
+
+        return xb, yb
