3f68294f345b0e67fe9edea21dcdf4efc92034a9
Davide Catucci <156239318+davidecatucci3@users.noreply.github.com>
2025-03-26 18:45:50 +0100

    Update hellaswag.py

--- hellaswag.py
+++ hellaswag.py
@@ -34,6 +34,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from transformers import GPT2LMHeadModel
+from model import GPT, GPTConfig
 
 DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), "hellaswag")
 
@@ -166,3 +167,66 @@ def evaluate(model_type, device):
             for i, end in enumerate(example["endings"]):
                 print(f"{i} (loss: {avg_loss[i].item():.4f}) {end}")
             print(f"predicted: {pred_norm}, actual: {label}")
+
+# helper function for HellaSwag eval
+# takes tokens, mask, and logits, returns the index of the completion with the lowest loss
+
+def get_most_likely_row(tokens, mask, logits):
+    # evaluate the autoregressive loss at all positions
+    shift_logits = (logits[..., :-1, :]).contiguous()
+    shift_tokens = (tokens[..., 1:]).contiguous()
+    flat_shift_logits = shift_logits.view(-1, shift_logits.size(-1))
+    flat_shift_tokens = shift_tokens.view(-1)
+    shift_losses = F.cross_entropy(flat_shift_logits, flat_shift_tokens, reduction='none')
+    shift_losses = shift_losses.view(tokens.size(0), -1)
+    # now get the average loss just for the completion region (where mask == 1), in each row
+    shift_mask = (mask[..., 1:]).contiguous() # we must shift mask, so we start at the last prompt token
+    masked_shift_losses = shift_losses * shift_mask
+    # sum and divide by the number of 1s in the mask
+    sum_loss = masked_shift_losses.sum(dim=1)
+    avg_loss = sum_loss / shift_mask.sum(dim=1)
+    # now we have a loss for each of the 4 completions
+    # the one with the lowest loss should be the most likely
+    pred_norm = avg_loss.argmin().item()
+    return pred_norm
+
+checkpoint = torch.load('log/model_05000.pt', map_location='cpu')
+
+model = GPT(GPTConfig())
+
+new_state_dict = {k.replace("_orig_mod.", ""): v for k, v in checkpoint['model'].items()}
+
+model.load_state_dict(new_state_dict)  
+model.eval() 
+
+tn = tiktoken.get_encoding('gpt2')
+
+num_correct_norm = 0
+num_total = 0
+
+for i, example in enumerate(iterate_examples("val")):
+    # render the example into tokens and labels
+    _, tokens, mask, label = render_example(example)
+    tokens = tokens.to('cpu')
+    mask = mask.to('cpu')
+    # get the logits
+
+    with torch.no_grad():
+        with torch.autocast(device_type='cpu', dtype=torch.bfloat16):
+            logits, loss = model(tokens)
+
+        pred_norm = get_most_likely_row(tokens, mask, logits)
+
+    num_total += 1
+    num_correct_norm += int(pred_norm == label)
+
+    # reduce the stats across all processes
+    num_total = torch.tensor(num_total, dtype=torch.long, device='cpu')
+    num_correct_norm = torch.tensor(num_correct_norm, dtype=torch.long, device='cpu')
+
+    num_total = num_total.item()
+    num_correct_norm = num_correct_norm.item()
+
+    acc_norm = num_correct_norm / num_total
+
+    print(f"HellaSwag accuracy: {num_correct_norm}/{num_total}={acc_norm:.4f}")
